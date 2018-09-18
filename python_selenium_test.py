d193361c18f0b5e7ff1838e3900a1c726e2ec4c1
yajirobe <ericlyeric@gmail.com>
2018-09-18 01:09:54 -0700

    first commit

---
+++ python_selenium_test.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+
+# Opening in Firefox
+# driver = webdriver.Firefox()
+# driver.get('https://www.google.ca')
+
+# Opening in Chrome
+driver = webdriver.Chrome()
+driver.get('https://www.google.ca')
